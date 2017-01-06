PureNexusProject-Legacy/android_packages_apps_PureSettings

Improve Heads up toggle [2/2] * Add multiuser support, provide a system setting to override heads up   showing. User can disable heads up within this one so we don't need to   mess with Settings.Global heads up dependencies and break Google wide   implementations like priority filtering etc.
PureNexus Edits:
*Adapted for Marshmallow/Nougat

src/com/pure/settings/fragments/HeadsUpSettings.java
@@ -22,6 +22,7 @@ import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceGroup;
 import android.provider.Settings;
@@ -43,7 +44,7 @@ import com.pure.settings.utils.Utils;
 import com.pure.settings.utils.PackageListAdapter;
 import com.pure.settings.utils.PackageListAdapter.PackageItem;
 
-import com.pure.settings.preferences.BaseGlobalSettingSwitchBar;
+import com.pure.settings.preferences.BaseSystemSettingSwitchBar;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -51,7 +52,7 @@ import java.util.List;
 import java.util.Map;
 
 public class HeadsUpSettings extends SettingsPreferenceFragment
-        implements BaseGlobalSettingSwitchBar.SwitchBarChangeCallback,
+        implements BaseSystemSettingSwitchBar.SwitchBarChangeCallback,
                 AdapterView.OnItemLongClickListener, Preference.OnPreferenceClickListener {
 
     private static final int DIALOG_DND_APPS = 0;
@@ -69,7 +70,7 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private Map<String, Package> mDndPackages;
     private Map<String, Package> mBlacklistPackages;
 
-    private BaseGlobalSettingSwitchBar mEnabledSwitch;
+    private BaseSystemSettingSwitchBar mEnabledSwitch;
 
     private ViewGroup mPrefsContainer;
     private View mDisabledText;
@@ -122,8 +123,8 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     public void onStart() {
         super.onStart();
         final SettingsActivity activity = (SettingsActivity) getActivity();
-        mEnabledSwitch = new BaseGlobalSettingSwitchBar(activity, activity.getSwitchBar(),
-                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, true, this);
+        mEnabledSwitch = new BaseSystemSettingSwitchBar(activity, activity.getSwitchBar(),
+                Settings.System.HEADS_UP_USER_ENABLED, true, this);
     }
 
     @Override
@@ -393,14 +394,16 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     }
 
     private boolean getUserHeadsUpState() {
-         return Settings.Global.getInt(getContentResolver(),
-                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED,
-                Settings.Global.HEADS_UP_ON) != 0;
+         return Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                Settings.System.HEADS_UP_USER_ON,
+                UserHandle.USER_CURRENT) != 0;
     }
 
     private void setUserHeadsUpState(int val) {
-         Settings.Global.putInt(getContentResolver(),
-                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, val);
+         Settings.System.putIntForUser(getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                val, UserHandle.USER_CURRENT);
     }
 
     private void updateEnabledState() {

roguesyko/packages_apps_ReaperSettings

Conflicts:
	src/com/pure/settings/preferences/BaseGlobalSettingSwitchBar.java
	src/com/pure/settings/preferences/BaseSystemSettingSwitchBar.java
	src/com/reaper/settings/fragments/HeadsUpSettings.java
	src/com/reaper/settings/preferences/BaseGlobalSettingSwitchBar.java

src/com/reaper/settings/fragments/HeadsUpSettings.java
@@ -17,6 +17,7 @@
 package com.reaper.settings.fragments;
 
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,12 +30,12 @@ import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
 import com.reaper.settings.utils.Utils;
 
-import com.reaper.settings.preferences.BaseGlobalSettingSwitchBar;
+import com.reaper.settings.preferences.BaseSystemSettingSwitchBar;
 
 public class HeadsUpSettings extends SettingsPreferenceFragment
-        implements BaseGlobalSettingSwitchBar.SwitchBarChangeCallback {
+        implements BaseSystemSettingSwitchBar.SwitchBarChangeCallback {
 
-    private BaseGlobalSettingSwitchBar mEnabledSwitch;
+    private BaseSystemSettingSwitchBar mEnabledSwitch;
 
     private ViewGroup mPrefsContainer;
     private View mDisabledText;
@@ -63,8 +64,8 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     public void onStart() {
         super.onStart();
         final SettingsActivity activity = (SettingsActivity) getActivity();
-        mEnabledSwitch = new BaseGlobalSettingSwitchBar(activity, activity.getSwitchBar(),
-                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, true, this);
+        mEnabledSwitch = new BaseSystemSettingSwitchBar(activity, activity.getSwitchBar(),
+                Settings.System.HEADS_UP_USER_ENABLED, true, this);
     }
 
     @Override
@@ -103,14 +104,16 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     }
 
     private boolean getUserHeadsUpState() {
-         return Settings.Global.getInt(getContentResolver(),
-                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED,
-                Settings.Global.HEADS_UP_ON) != 0;
+         return Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                Settings.System.HEADS_UP_USER_ON,
+                UserHandle.USER_CURRENT) != 0;
     }
 
     private void setUserHeadsUpState(int val) {
-         Settings.Global.putInt(getContentResolver(),
-                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, val);
+         Settings.System.putIntForUser(getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                val, UserHandle.USER_CURRENT);
     }
 
     private void updateEnabledState() {